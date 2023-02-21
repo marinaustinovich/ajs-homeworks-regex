5f37c70fe593a6cdba271f44beb03dcf92bf5551
Marina Ustinovich <marishka0509@mail.ru>
2023-02-21 17:13:35 +0300

    added test to phones.test.js

--- src/js/__tests__/phones.test.js
+++ src/js/__tests__/phones.test.js
@@ -5,7 +5,7 @@ test.each([
   ['+7 960 000 00 00', '+79600000000'],
   ['+86 000 000 0000', '+860000000000'],
   [89270000000, '+89270000000'],
-  ['+86*000*000*0000', '+860000000000']
+  ['+86*000*000*0000', '+860000000000'],
 ])(('should transfer phone numbers'), (data, expected) => {
   expect(transferPhoneNumberToTemplate(data)).toBe(expected);
 });
